be3a7ae8db592a84e7cbd41de8a9cc2657bb0579
Bkibathi25 <31283015+Bkibathi25@users.noreply.github.com>
2017-12-15 08:06:21 -0500

    Add files via upload

---
+++ Dragon/BackgroundDragon.java
@@ -0,0 +1,54 @@
+
+/**
+ * Write a description of class BackgroundDragon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BackgroundDragon
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int width;
+    private int length;
+    private Color topcolor;
+    private Color bottomcolor;
+    private Color topright;
+    private Color c;
+    /**
+     * Constructor for objects of class BackgroundDragon
+     */
+    public BackgroundDragon(int width, int height);
+    {
+        // initialise instance variables
+        this.width = width;
+        this.height = height;
+        topcolor = Color.BLUE;
+        topright = Color.YELLOW;
+        bottomcolor = Color.GREEN;
+        
+    }
+    public BackgroundDragon(int w, int h, Color.topcolor, Color.bottomcolor, )
+    {
+        
+        
+    }
+    
+    public BackgroundDragon(int w, int h, color )
+
+     
+        
+     
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
